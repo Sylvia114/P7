4d822e8065e4a3422b1ce871330d2660ac99da30
Sylvia M <mangomanguee@gmail.com>
2023-02-03 19:11:24 +0100

    Correction de Card et suppression de Banner

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -7,19 +7,16 @@ function Collapse({title, description}){
 
   const [open, setOpen] = useState(false);
 
-  function handleClick(){
-    setOpen(!open)
-  }
-
   return (
     <div className='container'>
 
         <div className="titleContainer">
         <h2 className='collapseTitle'>{title}</h2>
+
         <button type='button' onClick={() => setOpen(!open)} className='collapseButton'>
-          {open === true ? <img src={ArrowDown} alt="open" className='arrow' /> 
-          : <img src={ArrowUp} alt="close" className='arrow'/>}
+          {open === true ? <img src={ArrowDown} alt="open" className='arrow' /> : <img src={ArrowUp} alt="close" className='arrow'/>}
         </button>
+
         </div>
 
         <div className='collapseDescriptionContainer'>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,15 @@
 import { Link } from 'react-router-dom'
 import '../../components/Header/styles.css'
-import Banner from '../Banner'
 import logo from '../../assets/logo.png'
  
 function Header() {
     return (
         <header>
-        <Banner>            
+        <div>            
 			<Link to="/">
                 <img src={logo} alt="logo Kasa" className='logo'/>
             </Link>
-        </Banner>
+        </div>
         <nav>
             <Link to="/" className='navbar-home'>Accueil</Link>
             <Link to="/aboutUs" className='navbar-aboutUs'>A propos</Link>
